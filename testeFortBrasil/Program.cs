f9218cae440f1d53f74f68cfa74c29d07e79b142
Marcio Silva <marcioadsworks@gmail.com>
2019-12-22 13:18:57 -0300

    teste pronto

--- testeFortBrasil/Program.cs
+++ testeFortBrasil/Program.cs
@@ -10,6 +10,39 @@ namespace testeFortBrasil
     {
         static void Main(string[] args)
         {
+            Console.Write("Quantos telefones irá digitar? ");
+            int n = int.Parse(Console.ReadLine());
+            string[] telefones = new string[n];
+            string telefone ="";
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write((i + 1) + "º telefone: ");
+                telefone = Console.ReadLine();
+                telefones[i] = telefone;
+            }
+
+            foreach(String s in telefones)
+            {
+                Console.WriteLine(s);
+            }
+
+            int count =0;
+            foreach(String s in telefones)
+            {
+                if (telefones[0].Substring(0,1) == telefones[1].Substring(0,1))
+                {
+                    for(int i = 1;i < telefone.Length; i++)
+                    {
+                        if(telefones[0].Substring(0,i) == telefones[1].Substring(0,i))
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            count = count /telefones.Length;
+            Console.WriteLine(count);
+            Console.ReadLine();
         }
     }
 }
